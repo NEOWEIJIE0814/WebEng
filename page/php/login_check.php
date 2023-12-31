4ece9ce5547a1489a8d39350fa8d450937679453
NEOWEIJIE0814 <weijie081401@gmail.com>
2023-12-31 21:05:50 +0800

    change to matric no

--- page/php/login_check.php
+++ page/php/login_check.php
@@ -4,23 +4,23 @@ global $conn;
 include 'dbconnect.php';
 
 // Retrieve form data
-$user_id = $_POST['user_id'];
+$matricNo = $_POST['matricNo']; // Change the variable name here
 $password = $_POST['password'];
 
 // Query the database
-$sql = "SELECT matricNo, passwordHash FROM users WHERE matricNo = '$user_id'";
+$sql = "SELECT matricNo, passwordHash FROM users WHERE matricNo = '$matricNo'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   $row = $result->fetch_assoc();
-  $matricNo = $row['matricNo'];
+  $dbMatricNo = $row['matricNo'];
   $hashed_password = $row['passwordHash'];
 
   // Verify password
   if (password_verify($password, $hashed_password)) {
     // Password is correct
     // Check if matricNo is equal to 1
-    if ($matricNo == 1) {
+    if ($dbMatricNo == 1) {
       // Redirect to admin.html
       header("Location: ../admin.html");
 
@@ -35,7 +35,7 @@ if ($result->num_rows > 0) {
   }
 } else {
   // User not found
-  echo "Invalid user ID.";
+  echo "Invalid matriculation number."; // Change the error message here
 }
 
 // Close database connection (you can omit this if you want to reuse the connection in other parts of your script)
